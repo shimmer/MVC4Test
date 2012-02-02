599d365c9218ffc468ec08d13fa2d1efc526bdba
Shimmer HEFI <shimmer.gui@heritagefunds.ca>
2012-02-02 16:06:25 -0500

    As initialized.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC4Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult register()
+        {
+            //return Content("<div style='color:green;'>Register</div>");
+            //return Json();
+            return View("preRegister");
+            
+        }
+
+    }
+}
